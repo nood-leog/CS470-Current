0e9079b62d65a7c453eba68d093596a91cd5745e
Alex <116304896+nood-leog@users.noreply.github.com>
2025-04-22 11:40:51 -0700

    Update create_files_with_subdirs.sh

--- Lab1/create_files_with_subdirs.sh
+++ Lab1/create_files_with_subdirs.sh
@@ -4,6 +4,3 @@
 #Generates ten subdirectories within the main directory, named file101 to file110.
 #In each subdirectory, creates a .txt file named tuser501.txt to tuser510.txt.
 #Writes a unique programming language name (e.g. “Python”) into each .txt file
-
-
-#git test 3
