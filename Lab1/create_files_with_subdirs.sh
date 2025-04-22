a1cfaa2e389d7020811fc21ac1b0e4a579db71ef
Alex <116304896+nood-leog@users.noreply.github.com>
2025-04-22 11:13:03 -0700

    Create create_files_with_subdirs.sh

---
+++ Lab1/create_files_with_subdirs.sh
@@ -0,0 +1,6 @@
+#create_files_with_subdirs.sh
+
+#Creates a main directory with the current date and time as its name.
+#Generates ten subdirectories within the main directory, named file101 to file110.
+#In each subdirectory, creates a .txt file named tuser501.txt to tuser510.txt.
+#Writes a unique programming language name (e.g. “Python”) into each .txt file
