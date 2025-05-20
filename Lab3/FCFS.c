de2259a39ee080438fede1a0b4d573913b0c1d92
Alex Laptop <alexanderfboyceweb3@gmail.com>
2025-05-19 22:16:22 -0700

    mostly done

--- Lab3/FCFS.c
+++ Lab3/FCFS.c
@@ -6,47 +6,68 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-typedef struct {
+#define TIME_QUANTUM 4
+
+typedef struct 
+{
     int process_id;
     int arrival_time;
     int burst_time;
     int waiting_time;
-    int turnarround_time;
+    int turnaround_time;
 } Process;
 
-//fucntion prototypes !TODO!
+//prototypes
+void  sortProcessesByArrival(Process proc[], int n);
+void fcfs(Process proc[], int n);
+void RR(Process proc[], int n);
+void srtf(Process proc[], int n);
+void printProcesses(const Process proc[], int n);
+void turnAroundTime(Process proc[], int n);
 
-
-//-------------MAIN FUNCTION-------------
-int main ()
+int main() 
 {
-    //intialize process with ids, arrival time, burst time
-    Process proc[] = {{1, 0, 24}, {2, 0, 3}, {3,0,3}};
-    int n = sizeof(proc)/sizeof(proc[0]);
+    Process proc[] = 
+    {
+        {1, 0, 24, 0, 0},
+        {2, 0,  3, 0, 0},
+        {3, 0,  3, 0, 0}
+    };
+    int n = sizeof(proc) / sizeof(proc[0]);
 
-    printf("\nFirst come first serve scheduling\n");
+    //first come first served
+    printf("\nFirst Come First Served scheduling\n");
     fcfs(proc, n);
     printProcesses(proc, n);
 
+    //reset
+    for (int i = 0; i < n; i++) 
+    {
+        proc[i].waiting_time = proc[i].turnaround_time = 0;
+    }
 
-    printf("\nRound Robbin scheduling\n");
-    RR();
+    //round robin
+    printf("\nRound Robin scheduling (quantum = %d)\n", TIME_QUANTUM);
+    RR(proc, n);
     printProcesses(proc, n);
 
-    printf("\nShortest job first scheduling\n")
-    srtf(proc);
-    printProcesses(proc);
+    //reset
+    for (int i = 0; i < n; i++) 
+    {
+        proc[i].waiting_time = proc[i].turnaround_time = 0;
+    }
+
+    //shortest remaining time first
+    printf("\nShortest Remaining Time First scheduling\n");
+    srtf(proc, n);
+    printProcesses(proc, n);
 
     return 0;
 }
 
-
-//-------------CORE FUNCTIONS-------------
-//fcfs - First come first serve scheduling
-//rr - Round Robbin scheduling
-//srtf - Shortest job first scheduling
-
+//first come first served
 void fcfs(Process proc[], int n)
 {
     //sort processes by arrival time
@@ -64,7 +85,7 @@ void fcfs(Process proc[], int n)
     //trunaround time = waiting time + burst time - arrival time
     for (int i=0; i<n; i++)
     {
-        proc[i].turnarround_time = proc[i].waiting_time + proc[i].burst_time - proc[i].arrival_time;
+        proc[i].turnaround_time = proc[i].waiting_time + proc[i].burst_time - proc[i].arrival_time;
     }
 
     //print order of execution
@@ -76,40 +97,103 @@ void fcfs(Process proc[], int n)
     printf("\n");
 }
 
-
-//Ensure that SJF handles processes based on burst time
-//function to perform Shortest job first scheduling
-void srtf(Process proc[])
+//round robin
+void RR(Process proc[], int n) 
 {
-    
-}
-
-//RR uses time quantum for process scheduling.
-//fucntion to perform Round robin scheduling
-void RR()
-{
-    
-}
-
-
-//-------------HELPER FUNCTIONS-------------
-
-//Function to manually sort processes by arrival time
-void sortProcessesByArrival(Process proc[], int n)
-{
-    for (int i=0; i<n-1; i++)
+    int rem_bt[n], time = 0, done = 0;
+    for (int i = 0; i < n; i++) 
     {
-        for (int j=0; j<n-i-1; j++)
+        rem_bt[i] = proc[i].burst_time;
+    }
+
+    while (done < n) 
+    {
+        for (int i = 0; i < n; i++) 
         {
-            if (proc[j].arrival_time>proc[j+1].arrival_time)
+            if (proc[i].arrival_time <= time && rem_bt[i] > 0) 
             {
-                Process temp = proc[j+1];
-                proc[j] = proc [j+1];
-                proc [j+1] = temp;
+                int qt = (rem_bt[i] > TIME_QUANTUM ? TIME_QUANTUM : rem_bt[i]);
+                rem_bt[i] -= qt;
+                time     += qt;
+                if (rem_bt[i] == 0) 
+                {
+                    done++;
+                    proc[i].waiting_time = time
+                        - proc[i].arrival_time
+                        - proc[i].burst_time;
+                    proc[i].turnaround_time = proc[i].waiting_time
+                        + proc[i].burst_time;
+                }
             }
         }
     }
 }
+
+//shortest remaining time first
+void srtf(Process proc[], int n) 
+{
+    int rem_bt[n], complete = 0, time = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        rem_bt[i] = proc[i].burst_time;
+    }
+
+    while (complete < n) 
+    {
+        int idx = -1, min_bt = INT_MAX;
+        for (int i = 0; i < n; i++) 
+        {
+            if (proc[i].arrival_time <= time && rem_bt[i] > 0 && rem_bt[i] < min_bt) 
+            {
+                min_bt = rem_bt[i];
+                idx    = i;
+            }
+        }
+        if (idx == -1) 
+        {
+            time++;
+            continue;
+        }
+        rem_bt[idx]--;
+        time++;
+        if (rem_bt[idx] == 0) 
+        {
+            complete++;
+            proc[idx].waiting_time = time
+                - proc[idx].arrival_time
+                - proc[idx].burst_time;
+            if (proc[idx].waiting_time < 0)
+            {
+                proc[idx].waiting_time = 0;
+            }
+        }
+        
+    }
+    
+    for (int i = 0; i < n; i++)
+    {
+        proc[i].turnaround_time = proc[i].waiting_time + proc[i].burst_time;
+    }
+}
+
+
+//Function to manually sort processes by arrival time
+void sortProcessesByArrival(Process proc[], int n) 
+{
+    for (int i = 0; i < n-1; i++)
+    {
+        for (int j = 0; j < n-1-i; j++)
+        {
+            if (proc[j].arrival_time > proc[j+1].arrival_time) 
+            {
+                Process tmp = proc[j];
+                proc[j]     = proc[j+1];
+                proc[j+1]   = tmp;
+            }
+        }
+    }
+}
+
 
 //fucntion to turn around time
 void turnAroundTime(Process proc[], int n)
@@ -117,25 +201,21 @@ void turnAroundTime(Process proc[], int n)
     //turnaround time = waiting time + burst time
     for (int i=0; i<n; i++)
     {
-        proc[i].turnarround_time = proc[i].waiting_time + proc[i].burst_time;
+        proc[i].turnaround_time = proc[i].waiting_time + proc[i].burst_time;
     }
 }
 
-
-// Function to find the index of the process with the shortest remaining time
-int findNextProcess(Process proc[], int current_time) 
+//print processes
+void printProcesses(const Process proc[], int n) 
 {
-    return;
-}
-
-
-
-void printProcesses(Process proc[], int n)
-{
-    printf("Process ID\tArrival Time\tBurst Time\tWaiting Time\tTurnaround Time\n");
-    for (int i=0; i<n; i++)
+    printf("PID\tArr\tBurst\tWait\tTurn\n");
+    for (int i = 0; i < n; i++) 
     {
-        printf("%d\t\t%d\t\t%d\t\t%d\t\t%d\n", proc[i].process_id, proc[i].arrival_time, proc[i].burst_time, proc[i].waiting_time, proc[i].turnarround_time);
-
+        printf("%d\t%d\t%d\t%d\t%d\n",
+            proc[i].process_id,
+            proc[i].arrival_time,
+            proc[i].burst_time,
+            proc[i].waiting_time,
+            proc[i].turnaround_time);
     }
 }
